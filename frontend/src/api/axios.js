7d0164076606e6abea257bb132788a1fe47b2e22
woojin <ujinchoi03@gmail.com>
2026-01-21 18:32:08 +0900

    Design: Update browser title and favicon

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8080/api', // 백엔드 주소
+    baseURL: 'https://time-table-4qlh.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
